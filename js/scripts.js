ec4b54f4471abc355e611b05e259e59d72b2fd6f
Galin McMahon <GalinMcMahon@gmail.com>
2017-04-12 16:31:30 -0700

    fix the separation of logic, update some, but not all, css, return the grand total

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,11 @@
                               // BUSINESS LOGIC
                               // using bigness instead of size to avoid conflicts
-                  // OUR CONTRUCTOR:
+                  // OUR CONSTRUCTOR:
 function Pizza(bigness, toppings) {
   this.bigness = bigness;
   this.toppings = toppings;
 }
                   // OUR PROTOTYPE:
-
-// var sizeCost = 0;
-
 Pizza.prototype.cost = (function() {
   var totalCost = 0;
 
@@ -16,7 +13,6 @@ Pizza.prototype.cost = (function() {
     if (this.toppings[i].checked)
       totalCost = totalCost + 1.5;
   }
-
     if (this.bigness === "Small") {
       totalCost = totalCost +  8;
     } else if (this.bigness  === "Medium") {
@@ -26,39 +22,20 @@ Pizza.prototype.cost = (function() {
     } else if (this.bigness  === "Family") {
       totalCost = totalCost + 14;
     }
-
     return totalCost;
 });
-
-
-function placeOrder() {
+                                // UI LOGIC
+$(document).ready(function() {
+  $("#submit").click(function() {
     var bigness = document.getElementById('bigness').value;
-    var toppings = document.getElementsByName('toppings[]');
+    var toppings = document.getElementsByName('toppings');
 
     var myPizza = new Pizza(bigness, toppings);
     var totalCost = myPizza.cost();
-
-    document.getElementById('output').innerHTML = "Total Cost: $" + totalCost;
-    //alert(totalCost);
-}
-                              // UI LOGIC
-$(document).ready(function() {
-//
-event.preventDefault();
+    var price = totalCost.toFixed(2);
+    event.preventDefault();
+    $("#output h3").hide();
+    document.getElementById("Price").innerHTML = "Your total is: $" + price;
+    // alert(toppings);
+  });
 });
-//   $("form#toppings").submit(function(event) {
-//     $("input:checked").each(function(){
-//       topps.push($(this).val());
-//     });
-//   });
-//
-//   $("form#size").submit(function(event) {
-//       var bigness = $("#bigness").val();
-//       console.log(bigness);
-//       return bigness;
-//   });
-//
-//   var chooseToppings = $("form#topps").val();
-//   var newPizza = new Pizza("Large", "Pepperoni");
-//   $("ul.sidebar-nav").append("<li>" + chooseToppings + "</li>");
-// });
